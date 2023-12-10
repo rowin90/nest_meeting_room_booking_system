0f101caaaf19085f63b54d13a65e807a0fa659fc
饶驹 <raoju1580@qq.com>
2023-12-10 22:50:04 +0800

    ✨ feat: 抽离配置

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,18 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
 import { createTransport, Transporter } from 'nodemailer';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class EmailService {
   transporter: Transporter;
 
-  constructor() {
+  constructor(private configService: ConfigService) {
     this.transporter = createTransport({
-      host: 'smtp.qq.com',
-      port: 587,
+      host: configService.get('nodemailer_host'),
+      port: configService.get('nodemailer_port'),
       secure: false,
       auth: {
-        user: 'raoju1580@qq.com',
-        pass: 'bkwjkbztjtexbfde',
+        user: configService.get('nodemailer_auth_user'),
+        pass: configService.get('nodemailer_auth_pass'),
       },
     });
   }
@@ -21,7 +22,7 @@ export class EmailService {
     await this.transporter.sendMail({
       from: {
         name: '会议室预定系统',
-        address: 'raoju1580@qq.com',
+        address: this.configService.get('nodemailer_auth_user'),
       },
       to,
       subject,
